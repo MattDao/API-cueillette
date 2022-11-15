537593fb5fdb61a649bc94510dd1a931a36c4cda
MattDao <113097684+MattDao@users.noreply.github.com>
2022-11-15 16:27:22 +0100

    routes2

--- src/Routes/PlanteRoutes.ts
+++ src/Routes/PlanteRoutes.ts
@@ -8,5 +8,11 @@ planteRouter.get('/', (req, res) => {
     console.log('Planterouter');
     planteController.getAllPlantes(req, res);
 });
+planteRouter.get("/:id", (req, res) => planteController.getOnePlantById(req, res));
+planteRouter.post("/", (req, res) => planteController.createNewPlant(req, res));
+planteRouter.put("/:id", (req, res) => planteController.updateOnePlant(req, res));
+planteRouter.delete("/:id", (req, res) => planteController.deleteOnePlant(req, res));
+
+
 
 export default planteRouter;
--- src/services/PlanteServices.ts
+++ src/services/PlanteServices.ts
@@ -7,6 +7,30 @@ class PlanteService {
 
     return AppDataSource.query("SELECT * FROM listplants;");
   }
+  getOnePlantById(id: number): Promise<Plante> {
+    return AppDataSource.query(`SELECT name FROM Plante where id =${id}`);
+  }
+
+  createNewPlant(NewPlant: Plante): Promise<any> {
+    console.log(NewPlant.name);
+    return AppDataSource.query(
+      `INSERT INTO plante (name, category, quantity, rating, unitprice, url_pic) VALUES ('${NewPlant.name}', ${NewPlant.category}, ${NewPlant.quantity}, ${NewPlant.rating}, ${NewPlant.unitprice}, ${NewPlant.url_pic})`
+    );
+  }
+
+  updateOnePlant(id: number, changes: Plante): Promise<any> {
+    console.log(changes);
+    console.log(id);
+
+    return AppDataSource.query(
+      `UPDATE plant SET name ='${changes.name}',category = ${changes.category}, quantity =${changes.quantity}, rating =${changes.rating}, url_pic =${changes.url_pic} WHERE id= ${id}`
+    );
+  }
+
+  deleteOnePlant(id: number): Promise<any> {
+    console.log(id);
+    return AppDataSource.query(`DELETE FROM plante WHERE id = ${id}`);
+  }
 }
 
 export default PlanteService;
